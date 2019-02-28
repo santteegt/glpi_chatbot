7c547a73a14e5e5dfe6f07ad814d2773cdf27c1d
renan <renangfz@hotmail.com>
2019-02-28 13:00:39 -0500

    Added client-web

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+// import { Widget } from 'react-chat-widget';
+
+// import 'react-chat-widget/lib/styles.css';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {      
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <Widget />
+  //     </div>
+  //   );
+  // }
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   );
+  // }
+}
+
+export default App;
